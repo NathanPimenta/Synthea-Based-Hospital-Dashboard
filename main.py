927482e96c2ac4dae022da25694287edbc6845e0
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-08-30 10:27:26 +0530

    Some minor setup changes

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from hospital-readmission-prediction-and-analysis!")
+"""To handle the backend API Calls and management of data"""
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from collections import Counter
+from typing import List, DefaultDict
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+app.add_middleware(
+
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"Message": "Welcome to hospital readmission prediction and analysis"}
+
+@app.get("/dashboard")
+async def root():
+    return {"Message": "Interactive dashboard for hospital readmission prediction and analysis as well as insights"}
